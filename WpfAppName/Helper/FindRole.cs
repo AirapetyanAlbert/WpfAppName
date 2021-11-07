5f695e9afd26138deab3cd91e85bba36c1ef8ac9
Dzhamgar <dzhamgar10@mail.ru>
2021-11-07 21:45:10 +0300

    first

---
+++ WpfAppName/Helper/FindRole.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using WpfApplDemo2018.ViewModel;
+namespace WpfApplDemo2018.Helper
+{
+    public class FindRole
+    {
+        int id;
+        public FindRole(int id)
+        {
+            this.id = id;
+        }
+        public bool RolePredicate(Model.Role role)
+        {
+            return role.Id == id;
+        }
+    }
+}
---
+++ WpfAppName/View/WindowRole.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplDemo2018.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowRole.xaml
+    /// </summary>
+    public partial class WindowRole : Window
+    {
+        public WindowRole()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            WindowNewRole wnRole = new WindowNewRole
+            {
+                Title = "Новая должность",
+                Owner = this
+            };
+
+            int maxIdRole = wnRole.MaxId() + 1;
+            Model.Role role = new Model.Role
+            {
+                Id = maxIdRole
+            };
+            wnRole.DataContext = role;
+            if (wnRole.ShowDialog() == true)
+            {
+                wnRole.ListRole.Add(role);
+            }
+        }
+
+
+    }
+
+
+}
